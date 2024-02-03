4db9a78cbbe360a751c74658344c23e8ef7bf060
alfiy <qdalfiy@gmail.com>
2024-02-03 16:55:34 +0800

    Update openvpn.sh

--- openvpn.sh
+++ openvpn.sh
@@ -3,10 +3,10 @@
 sudo apt update && \
 # 安装依赖
 sudo apt install -y gcc make liblz4-dev libssl-dev liblzo2-dev libpam0g-dev && \
-wait
+
 # 进入下载目录
 cd ~/Downloads/
-
+wait
 # 下载openvpn源码包
 wget https://swupdate.openvpn.org/community/releases/openvpn-2.5.1.tar.gz && \
 
